7591810e18831ed8efcb3720a77c51f7da6ed320
unknown <madreskimani001@gmail.com>
2025-09-16 10:47:39 +0300

    add array search practice file

---
+++ arrayS.js
@@ -0,0 +1,14 @@
+//Array search
+
+const county = ["Kiambu","Nakuru","Mombasa","Narok"];
+
+console.log(county.indexOf("Nakuru")+1);
+console.log(county.includes("Kiambu"));
+
+console.log(county.sort());
+console.log(county.reverse());
+
+const nums = [9,1,4,2,3,5,8,7,6];
+console.log(nums.sort());
+console.log(nums.reverse());
+//use toSorted() and toReversed() methods if you dont wish to alter with the original array
