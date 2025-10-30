d39c3137d50bb6c35f84b1f78eb0164a31f16d53
willy <madreskimani001@gmail.com>
2025-10-30 14:39:58 +0300

    tuples

--- tuples.py
+++ tuples.py
@@ -1,4 +1,5 @@
 #Tuples are used to store multiple items just like list,set & dictionary
+  #packing values into a tuple
 students = ("Joyce","Alice","Maina","Bob","Bob","Agnes","Omondi")
 print(students)
 #tuples are ordered and unchangeable and allow duplication
@@ -13,3 +14,20 @@ print(students[2:5]) #prints within a range
 #it can contain different data types
 tuple =("name",23,True)
 print(tuple)
+
+#unpacking from a tuple
+(no1,no2,no3,*no4) = students
+print("Student No.1 is ",no1)
+print("The rest are ",no4)  # an asterisk collectes the rest as a list
+
+#looping through a tuple
+t =0
+print("The loop:")
+while t < len(students):
+   print(students[t])
+   t += 1
+#for loop can also be used to loop a tuple
+"""for x in students:
+       print(x)"""
+
+#jioning tuples is done using + operator 
