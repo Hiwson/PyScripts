f4ac8c7a2ef04942b5c68e316494270bd6fa0a48
Willy <160387904+Hiwson@users.noreply.github.com>
2025-11-11 13:06:14 +0000

    just commenting my code

--- guessing_game.py
+++ guessing_game.py
@@ -1,14 +1,16 @@
 import random
 import math
-
+	# getting user input
 player = input("Whats your name lad??......\t")
 do{
 playerNo = input("Enter a number between 1-10:.....\t")
+	# generating a random number
 winningNo = random.random()
 winningNo = math.floor(winningNo*10)
 print(winningNo)
-
+	# type conversion from string to integer
 pick = int(playerNo)
+	# conditioning to get the miss range
 if pick < winningNo:
 	print("Your number was lesser than winning number!!!!")
 	print(f"Oooops!!! {player} lost mahn----try again")
